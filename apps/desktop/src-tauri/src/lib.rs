197117af719f593a0a2da5c96509fea9f43ef555
nidalJaafar <nidaljaafar.nj@gmail.com>
2026-04-15 03:24:03 +0300

    Set window icon programmatically at startup for Linux dev mode

--- apps/desktop/src-tauri/src/lib.rs
+++ apps/desktop/src-tauri/src/lib.rs
@@ -1,7 +1,17 @@
+use tauri::Manager;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_sql::Builder::default().build())
+        .setup(|app| {
+            if let Some(window) = app.get_webview_window("main") {
+                let icon_bytes = include_bytes!("../icons/icon.png");
+                let icon = tauri::image::Image::from_bytes(icon_bytes)?;
+                window.set_icon(icon)?;
+            }
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
